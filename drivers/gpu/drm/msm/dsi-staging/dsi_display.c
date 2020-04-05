1b009c46c7af2185c927fc3c48059995efa8ff7a
Demon000 <demonsingur@gmail.com>
2020-04-05 22:41:58 +0200

    drm/msm/dsi-staging: Create empty attributes group
    Change-Id: I974643cb245f76e66b208f7e18601cbb3d90ae62

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -5520,7 +5520,7 @@ static int dsi_display_sysfs_init(struct dsi_display *display)
 
 	rc = sysfs_create_group(&dev->kobj, &display_fs_attrs_group);
 	if (rc)
-		pr_debug("failed to create display device attributes");
+		pr_err("failed to create display device attributes");
 
 	if (display->panel->panel_mode == DSI_OP_CMD_MODE)
 		rc = sysfs_create_group(&dev->kobj,
